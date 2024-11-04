9d57f83733b5a4a952189a0f10d440d6547e1906
Jovian Dsouza <dsouzajovian123@gmail.com>
2024-11-04 23:13:04 +0530

    feat: add rainbow kit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { RecoilProvider } from "@/components/providers/RecoilProvider";
+import { Providers } from "@/components/providers/RainbowKitProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased vsc-initialized`}
       >
+        <Providers>
           <RecoilProvider>
             {children}
           </RecoilProvider>
+        </Providers>
       </body>
     </html>
   );
---
+++ components/providers/RainbowKitProvider.tsx
@@ -0,0 +1,32 @@
+   'use client';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  RainbowKitProvider,
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { zeroGTestnet } from '@/data/chain';
+
+
+const wagmiConfig = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [zeroGTestnet],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
